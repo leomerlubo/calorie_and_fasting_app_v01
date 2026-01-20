f9e39bbc1e67b2ca65e7929de46215a4c76d1db2
/ L e e <64281675+leomerlubo@users.noreply.github.com>
2026-01-21 03:06:13 +0800

    Add Capacitor setup for Android Studio

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.example.selfloveapp",
+  appName: "selflove-app",
+  webDir: "dist",
+  bundledWebRuntime: false
+};
+
+export default config;
